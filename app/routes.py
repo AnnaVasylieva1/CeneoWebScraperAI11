59738a21977810330b864ac6ef773a9303f12c2c
AnnaVasylieva1 <anna.g.vassilieva@gmail.com>
2024-05-22 19:41:59 +0200

    2024-06-22

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from app import app 
+import json 
+import pandas as pd 
+import numpy as np 
+import os
 from flask import render_template, request, url_for, redirect
 import requests
 from bs4 import BeautifulSoup
+from app import utils 
+from app.utils import extract_content, rate, selectors, tr, translate
 @app.route('/')
 @app.route('/index')
 def index():
@@ -15,19 +21,62 @@ def extract():
         response = requests.get(url)
         if response.status_code == requests.codes['ok']:
             page_dom = BeautifulSoup(response.text, "html.parser")
-            try:
-                opinions_count = page_dom.select_one('a.product-review__link > span').get_text().strip()
-            except AttributeError:
-                opinions_count = 0
+            opinions_count = extract_content(page_dom,'a.product-review__link > span')
             if opinions_count:
+                url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+                all_opinions = []
+                while(url):
+                    response = requests.get(url)
+                    response.status_code
+                    page_dom = BeautifulSoup(response.text, "html.parser")
+                    product_name = extract_contet(page_dom, "h1")
+                    opinions = page_dom.select("div.js_product-review")
+                    for opinion in opinions:
+                        single_opinion = {
+                            key: extract(opinion, *value)
+                                for key, value in selectors.items()
+                        }
+                        for key, value in transformations.items():
+                            single_opinion[key] = value(single_opinion[key])
+                        all_opinions.append(single_opinion)
+                    try:
+                        url = 'https://www.ceneo.pl'+extract(page_dom, "a.pagination__next", "href")
+                    except TypeError:
+                        url = None
+                if not os.path.exists("app/data/opinions"):
+                    os.mkdir("app/data/opinions")
+                with open (f"app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+                    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+                max_score = 5 
+                opinions = pd.DataFrame.from_dict(all_opinions)
+                opinions.score = opinions.score.apply(lambda s: round(s*max_score,1))
+                statistics ={
+                    "product_id" : product_id,
+                    "product_name" : product_name,
+                    "pros_count" : int(opinions.pros.astype(bool).sum),
+                    "cons_count" : int(opinions.cons.astype(bool).sum),
+                    "average_score" : opinions.score.mean().round(3),
+                    "score_distribution" : opinions.score.value_counts().reindex(np.arange(0.5,5.5,0.5)),
+                    "recomendation_distribution" : opinions.recommendation.value_counts(dropna=False).reindex([1,np.nan,0])
+                }
+                if not os.path.exists("app/data/opinions"):
+                    os.mkdir("app/data/opinions")
+                with open (f"app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+                    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
                 return redirect(url_for('product', product_id=product_id))
+
             return render_template("extract.html", error = "Product has no opinions")
-        render_template("extract.html", error = "Poduct does not exist")
     return render_template("extract.html")
 
 @app.route('/products')
 def products():
-    return render_template("products.html")
+    products_list = [filename.split(".")[0] for filename in os.listdir("app/data/opinions")]
+    product = []
+    for product_id in products_list:
+        with open (f"app/data/statistics/{product_id}.json", "r", encoding="UTF-8") as jf:
+            statistics = json.load(jf)
+            product.append(statistics)
+    return render_template("products.html", products=products)
 
 @app.route('/author')
 def author():
@@ -35,5 +84,8 @@ def author():
 
 @app.route('/product/<product_id>')
 def product(product_id):
-    return render_template("product.html", product_id=product_id)
-
+    if not os.path.exists("app/data/opinions"):
+        with open (f"app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+            opinions = json.load(jf)
+        return render_template("product.html", product_id=product_id)
+    return redirect(url_for("extract"))
